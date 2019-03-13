f8915df14000571877828a739c4a56ae557c2dbf
konradusko <konradusko1337@gmail.com>
2019-03-13 19:01:14 +0100

    work work

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ firebase.auth().onAuthStateChanged(function(user) {
       document.getElementById("log-page").style.display = "none";
       document.getElementById("mess-page").style.display = "flex";
       window.alert("udalo sie zalogowac");
+      let user =  firebase.auth().currentUser;
+      let name, email, photoUrl;
+
+if (user != null) {
+  name = user.displayName;
+  email = user.email;
+  photoUrl = user.photoURL;
+}
+
     } else {
       // No user is signed in.
       document.getElementById("log-page").style.display = "flex";
@@ -11,20 +20,39 @@ firebase.auth().onAuthStateChanged(function(user) {
   
     }
   });
-
+document.getElementById("input_login").addEventListener("click", login)
   function login(){
       let userEmail = document.getElementById("login-mail").value;
       let userPassword = document.getElementById("login_password").value;
 
       firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
         // Handle Errors here.
+
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
 
-        window.alert("Error :" + errorMessage)
+     document.getElementById("span_login").innerHTML = errorMessage;
       });
   }
   function logOut(){
     firebase.auth().signOut()
+  }
+  document.getElementById("input_register").addEventListener("click", register);
+  function register(){
+ let userRegisterEmail = document.getElementById("register_email").value;
+ let userRegisterPassOne = document.getElementById("register_password_one").value;
+ let userRegisterPasstwo = document.getElementById("register_password_two").value;
+ if(userRegisterPassOne === userRegisterPasstwo){
+    firebase.auth().createUserWithEmailAndPassword(userRegisterEmail, userRegisterPassOne).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+           document.getElementById("span_register").innerHTML = errorMessage;
+      });
+ }else{
+    document.getElementById("span_register").innerHTML = "Hasła nie są takie same."
+ }
+    
   }
